9f9de887ed8aa3a9543f7df7d46cb7aee221a275
rpingali10 <vp387@exeter.ac.uk>
2025-11-17 04:11:12 +0000

    new pipeline functions

--- course/intro/pipeline_functions.py
+++ course/intro/pipeline_functions.py
@@ -7,22 +7,21 @@ import plotly.express as px
 
 def plot_scatter(df, x_name, y_name):
     """Given a dataframe containing numeric columns specified by x_name and y_name
-    return a plotly express scatterplot"""
+    return a plotly express scatterplot."""
     fig = px.scatter(df, x=x_name, y=y_name)
     return fig
 
 
 def calculate_correlation(df, x1, x2):
-    """Given a dataframe containing numeric columns specified by x_name and y_name
-    return two objects (numbers), the first is the pearson correlation coefficient, the second 
-    the significance of this estimate"""
-    corr_coef, p_value = pearsonr(df[x_name], df[y_name])
+    """Given a dataframe containing numeric columns x1 and x2
+    return the Pearson correlation coefficient and p-value."""
+    corr_coef, p_value = pearsonr(df[x1], df[x2])
     return corr_coef, p_value
 
 
 def fit_regression(df, x_name, y_name):
     """Given a dataframe containing numeric columns specified by x_name and y_name
-    return the stats models OLS fit of a regression model of y on x"""
+    return the statsmodels OLS fit of a regression model of y on x."""
     X = sm.add_constant(df[x_name])
     y = df[y_name]
     model = sm.OLS(y, X).fit()
@@ -30,24 +29,23 @@ def fit_regression(df, x_name, y_name):
 
 
 def filter_data(df, year):
-    """Given a dataframe of various rows including a column 'Year' and an integer year
-    return a dataframe containing only those rows where the value in this column is less
-    than the value of the supplied year"""
+    """Given a dataframe including a column 'Year' and an integer year,
+    return rows where the value in 'Year' is less than the supplied year."""
     return df[df['Year'] < year]
 
 
 def tyler_viglen():
-    """Create the data used at
-    https://www.tylervigen.com/
-    spurious/correlation/19524_kerosene-used-in-india_correlates-with_the-divorce-rate-in-maine"""
+    """Create the Tyler Vigen data."""
     array_1 = np.array([
       227, 238.806, 220.93, 220.358, 216.652, 198.424, 198.587, 201.298,
       198.333, 196.481, 197.041, 189.078, 183, 166, 157, 154, 149, 128,
       88, 76.9508, 59.2101, 40.4265, 34.1946
-      ])
+    ])
     array_2 = np.array([
       5.1, 5, 4.7, 4.6, 4.4, 4.3, 4.1, 4.2, 4.2, 4.2, 4.1, 4.2, 4.2, 3.9,
-      3.96973, 3.58172, 3.42805, 3.42852, 3.22627, 3.19709, 3.033, 2.40567, 2.72837
-      ])
+      3.96973, 3.58172, 3.42805, 3.42852, 3.22627, 3.19709, 3.033,
+      2.40567, 2.72837
+    ])
     years = np.arange(1999, 2022)
-    return pd.DataFrame({'Year': years, 'Kerosene': array_1, 'DivorceRate': array_2})
+    return pd.DataFrame({'Year': years, 'Kerosene': array_1,
+                         'DivorceRate': array_2})
