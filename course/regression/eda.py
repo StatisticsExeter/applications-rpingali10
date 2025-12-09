03c5cc0790aaf082eff8d640d0dd950c08c55b0f
rpingali10 <vp387@exeter.ac.uk>
2025-12-09 17:34:55 +0000

    eda.py regression

--- course/regression/eda.py
+++ course/regression/eda.py
@@ -10,7 +10,9 @@ def _boxplot(df, x_var, y_var, title):
     """Given a data frame 'df' containing categorical variable 'x_var'
     and outcome variable 'y_var' produce a box plot of the distribution of the y_variable
     for different levels of y_var. The box plot should have title 'title'"""
-    return 0
+    import plotly.express as px
+    fig = px.box(df, x=x_var, y=y_var, points='all', title=title)
+    return fig
 
 
 def boxplot_age():
