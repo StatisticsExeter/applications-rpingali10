e5c8425538f2daec4f72f0afa4f9c4cd1f0fdb3b
rpingali10 <vp387@exeter.ac.uk>
2025-12-09 13:01:51 +0000

    split_test_train.py

--- course/supervised_classification/split_test_train.py
+++ course/supervised_classification/split_test_train.py
@@ -19,12 +19,19 @@ def test_and_train():
 def split_data(base_data_path, X_train_path, y_train_path, X_test_path, y_test_path):
     df = pd.read_csv(base_data_path)
 
-    y = df["built_age"]
+    df = df.dropna(subset=['built_age'])
 
-    X = df.drop(columns=["built_age"])
+    y = df['built_age']
+    X = df.drop(columns=['built_age'])
+
+    X = X.fillna(X.mean(numeric_only=True))
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42, stratify=y
+        X,
+        y,
+        test_size=0.3,
+        random_state=42,
+        stratify=y
     )
 
     X_train.to_csv(X_train_path, index=False)
