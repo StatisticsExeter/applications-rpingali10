61d2d3c88c681be661a7bf05867b9b4e2bfe7966
rpingali10 <vp387@exeter.ac.uk>
2025-12-09 13:46:06 +0000

    predict.py

--- course/supervised_classification/predict.py
+++ course/supervised_classification/predict.py
@@ -2,26 +2,19 @@ import joblib
 import pandas as pd
 from course.utils import find_project_root
 
-
 def predict(model_path, X_test_path, y_pred_path, y_pred_prob_path):
-
     model = joblib.load(model_path)
-
     X_test = pd.read_csv(X_test_path)
+    
 
     y_pred = model.predict(X_test)
-    y_pred_series = pd.Series(y_pred, name='predicted_built_age')
-    y_pred_series.to_csv(y_pred_path, index=False)
-
-    if hasattr(model, "predict_proba"):
-        y_pred_prob = model.predict_proba(X_test)
-        prob_df = pd.DataFrame(y_pred_prob, columns=model.classes_)
-        prob_df.to_csv(y_pred_prob_path, index=False)
-    else:
-        pd.DataFrame().to_csv(y_pred_prob_path, index=False)
-
-    return None
-
+    pd.Series(y_pred, name='predicted_built_age').to_csv(y_pred_path, index=False)
+    
+ 
+    y_pred_prob = model.predict_proba(X_test)
+    class_names = model.classes_
+    y_pred_prob_df = pd.DataFrame(y_pred_prob, columns=[f'prob_{cls}' for cls in class_names])
+    y_pred_prob_df.to_csv(y_pred_prob_path, index=False)
 
 def pred_lda():
     base_dir = find_project_root()
@@ -30,7 +23,6 @@ def pred_lda():
     y_pred_path = base_dir / 'data_cache' / 'models' / 'lda_y_pred.csv'
     y_pred_prob_path = base_dir / 'data_cache' / 'models' / 'lda_y_pred_prob.csv'
     predict(model_path, X_test_path, y_pred_path, y_pred_prob_path)
-
 
 def pred_qda():
     base_dir = find_project_root()
