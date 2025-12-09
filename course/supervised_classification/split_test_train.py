6a842303ed0e6aa9ab441d00bbf4ffa7880020f9
rpingali10 <vp387@exeter.ac.uk>
2025-12-09 18:11:55 +0000

    split_test_train.py

--- course/supervised_classification/split_test_train.py
+++ course/supervised_classification/split_test_train.py
@@ -26,12 +26,15 @@ def split_data(base_data_path, X_train_path, y_train_path, X_test_path, y_test_p
 
     X = X.fillna(X.mean(numeric_only=True))
 
-    X_train, X_test, y_train, y_test = train_test_split(X,y,
-      test_size=0.3,
-      random_state=42,
-      stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.3,
+        random_state=42,
+        stratify=y
+    )
 
     X_train.to_csv(X_train_path, index=False)
-    y_train.to_csv(y_train_path, index=False)
     X_test.to_csv(X_test_path, index=False)
+    y_train.to_csv(y_train_path, index=False)
     y_test.to_csv(y_test_path, index=False)
