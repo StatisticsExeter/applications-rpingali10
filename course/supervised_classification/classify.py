21635e257ee17e97c4df9d6641cecb307685b427
rpingali10 <vp387@exeter.ac.uk>
2025-12-09 13:08:57 +0000

    classify.py

--- course/supervised_classification/classify.py
+++ course/supervised_classification/classify.py
@@ -7,25 +7,35 @@ from course.utils import find_project_root
 def fit_classifier(X_train_path, y_train_path, model_path, classifier):
     X_train = pd.read_csv(X_train_path)
     y_train = pd.read_csv(y_train_path)['built_age']
+
     classifier.fit(X_train, y_train)
+
     joblib.dump(classifier, model_path)
 
 
 def fit_lda():
     base_dir = find_project_root()
+
     X_train_path = base_dir / 'data_cache' / 'energy_X_train.csv'
     y_train_path = base_dir / 'data_cache' / 'energy_y_train.csv'
     model_path = base_dir / 'data_cache' / 'models' / 'lda_model.joblib'
-    """Initialise a linear discriminant analysis classifierr called classifier which can be
-    passed to fit_classifier and fitted by classifer.fit()"""
+
+    classifier = LinearDiscriminantAnalysis()
+
     fit_classifier(X_train_path, y_train_path, model_path, classifier)
+
+    return None
 
 
 def fit_qda():
     base_dir = find_project_root()
+
     X_train_path = base_dir / 'data_cache' / 'energy_X_train.csv'
     y_train_path = base_dir / 'data_cache' / 'energy_y_train.csv'
     model_path = base_dir / 'data_cache' / 'models' / 'qda_model.joblib'
-    """Initialise a linear discriminant analysis classifierr called classifier which can be
-    passed to fit_classifier and fitted by classifer.fit()"""
+
+    classifier = QuadraticDiscriminantAnalysis()
+
     fit_classifier(X_train_path, y_train_path, model_path, classifier)
+
+    return None
