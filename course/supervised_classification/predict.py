ced5ba9afa2ebd6cc5af43e142f6ef966ca38aed
rpingali10 <vp387@exeter.ac.uk>
2025-12-10 15:55:50 +0000

    predict.py

--- course/supervised_classification/predict.py
+++ course/supervised_classification/predict.py
@@ -6,18 +6,15 @@ from course.utils import find_project_root
 def predict(model_path, X_test_path, y_pred_path, y_pred_prob_path):
     model = joblib.load(model_path)
     X_test = pd.read_csv(X_test_path)
-
+    """Form an object y_pred containing a list of your classifer predictions"""
     y_pred = model.predict(X_test)
     y_pred_series = pd.Series(y_pred, name='predicted_built_age')
     y_pred_series.to_csv(y_pred_path, index=False)
-
-    if y_pred_prob_path != y_pred_path:
-        y_pred_prob = model.predict_proba(X_test)
-        prob_df = pd.DataFrame(
-            y_pred_prob,
-            columns=[f"prob_{c}" for c in model.classes_]
-        )
-        prob_df.to_csv(y_pred_prob_path, index=False)
+    """Form an object y_pred_prob containing a list of
+    the probability of your classier predictions"""
+    y_pred_prob = model.predict_proba(X_test)[:, 1]
+    y_pred_prob_series = pd.Series(y_pred_prob, name='predicted_built_age')
+    y_pred_prob_series.to_csv(y_pred_prob_path, index=False)
 
 
 def pred_lda():
